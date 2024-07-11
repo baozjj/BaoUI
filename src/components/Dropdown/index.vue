4dab96a0a984256a1512b57c9898d31d4d62c892
baozj <1670370148@qq.com>
2024-07-11 23:06:41 +0800

    feat: dropdown基础功能

---
+++ src/components/Dropdown/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class='v-dropdown'>
+    <Tooltip
+      :trigger="trigger"
+      :placement="placement"
+      :popper-options="popperOptions"
+      :open-delay="openDelay"
+      :close-delay="closeDelay"
+      @visible-change="visibleChange"
+      ref="tooltipRef"
+    >
+      <slot></slot>
+      <template #content>
+          <ul class="v-dropdown__menu">
+            <template v-for="item in menuOptions" :key="item.key">
+              <li
+                v-if="item.divided"
+                role="separator"
+                class="divided-placeholder"
+              />
+
+              <li
+                class="v-dropdown__item"
+                @click="itemClick(item)"
+                :class="{'is-disabled': item.disabled, 'is-divied': item.divided }"
+                :id="`dropdown-item-${item.key}`"
+              >
+                {{  item.label }}
+              </li>
+
+            </template>
+          </ul>
+      </template>
+    </Tooltip>
+  </div>
+</template>
+
+<script lang='ts' setup >
+import { ref } from 'vue'
+import type { DropdownProps, DropdownInstance, DropdownEmits, MenuOption } from './types'
+import Tooltip from '../Tooltip/index.vue'
+import type { TooltipInstance } from '../Tooltip/types';
+
+const props = defineProps<DropdownProps>()
+const emits = defineEmits<DropdownEmits>()
+
+const tooltipRef = ref<TooltipInstance>()
+
+const visibleChange = (e: boolean) => {
+  emits('visible-change', e)
+}
+
+const itemClick = (e: MenuOption) => {
+  if (e.disabled) {
+    return
+  }
+  emits('select', e)
+}
+</script>
+
+<style scoped lang='stylus'>
+</style>
